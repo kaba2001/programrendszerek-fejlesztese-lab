6e935df38ee6347eb8bc82b0f5c3d74a8973ecfd
Kaba <kbenko43@gmail.com>
2026-04-05 11:18:53 +0200

    init BE

---
+++ start-dev-envs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+TARGET_DIR="./"
+FE_PATH=''
+BE_PATH='netbank-backend-spring'
+SESSIONNAME="netbank-app"
+
+# Default host
+HOST="localhost"
+
+# Check for 'ln' argument
+if [ "$1" == "ln" ]; then
+    # Get local IP address
+    HOST=$(hostname -I | awk '{print $1}')
+    echo "Using local network IP: $HOST"
+else
+    echo "Using localhost"
+fi
+
+# Check if session exists
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ]; then
+    # Create new session with a window
+    tmux new-session -s $SESSIONNAME -n dev -d
+
+    # Split window vertically
+    tmux split-window -v -t $SESSIONNAME
+
+    # In second pane (bottom), cd to be and run Django server
+    tmux send-keys -t $SESSIONNAME:0.0 "cd $TARGET_DIR/$BE_PATH" C-m
+    tmux send-keys -t $SESSIONNAME:0.0 "./mvnw spring-boot:run" C-m
+
+    # In first pane (top), cd to fe and run ng serve
+    # tmux send-keys -t $SESSIONNAME:0.1 "cd $TARGET_DIR/$FE_PATH" C-m
+    # tmux send-keys -t $SESSIONNAME:0.1 "ng serve --host $HOST" C-m
+
+    # Back-end editor window
+    tmux new-window -n Back-end -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME:1 "cd $TARGET_DIR/$BE_PATH" C-m
+    tmux send-keys -t $SESSIONNAME:1 "source .venv/bin/activate" C-m
+    tmux send-keys -t $SESSIONNAME:1 "nvim ." C-m
+
+    # Front-end editor window
+    # tmux new-window -n Fron-end -t $SESSIONNAME
+    # tmux send-keys -t $SESSIONNAME:2 "cd $TARGET_DIR/$FE_PATH" C-m
+    # tmux send-keys -t $SESSIONNAME:2 "nvim ." C-m
+
+    # Select the first editor window by default
+    tmux select-window -t $SESSIONNAME:0
+fi
+
+# Attach to session
+tmux attach -t $SESSIONNAME
